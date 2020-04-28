e2d08ddf07430330dbbd8ea2fd5966dedf292694
chunyenHuang <chunyenHuang@users.noreply.github.com>
2020-04-28 00:31:58 +0000

    deploy: 87513b2c192f48ca5fd2e1dd327effb19a7b9da8

---
+++ precache-manifest.2e8ce06651d30e6a86b3c26fae0d70a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d944e1643b5c044b96c1300e54184f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27bb8c8fa00d2d63de5e",
+    "url": "/static/css/main.1122466b.chunk.css"
+  },
+  {
+    "revision": "963b592fa8ad052c0e6f",
+    "url": "/static/js/2.ed30ff0f.chunk.js"
+  },
+  {
+    "revision": "480089e554fcb0ef7371718404a4da41",
+    "url": "/static/js/2.ed30ff0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27bb8c8fa00d2d63de5e",
+    "url": "/static/js/main.66f3f80a.chunk.js"
+  },
+  {
+    "revision": "c20a2c14bc8a0e9b27ce",
+    "url": "/static/js/runtime-main.e8297b11.js"
+  }
+]);
