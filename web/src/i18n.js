a3ef0a60cc070c4a3331adfc2c122edd1f2ba0a2
John Huang <little78926@gmail.com>
2020-04-27 18:03:23 -0700

    Fix invalid lang

--- web/src/i18n.js
+++ web/src/i18n.js
@@ -2,16 +2,18 @@ import i18n from 'i18next';
 import Backend from 'i18next-xhr-backend';
 import { initReactI18next } from 'react-i18next';
 
+const defaultLng = 'zh-TW';
+
 i18n
   .use(Backend)
   .use(initReactI18next)
   .init({
-    lng: 'zh',
+    lng: defaultLng,
     backend: {
       /* translation file path */
       loadPath: `${process.env.PUBLIC_URL}/assets/i18n/{{ns}}/{{lng}}.json`,
     },
-    fallbackLng: 'zh-TW',
+    fallbackLng: defaultLng,
     debug: process.env.NODE_ENV === 'development',
     /* can have multiple namespace, in case you want to divide a huge translation into smaller pieces and load them on demand */
     ns: ['translations'],
