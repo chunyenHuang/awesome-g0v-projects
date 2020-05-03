41c62538c6d6816adb76cb084e10133a980fe232
John Huang <little78926@gmail.com>
2020-05-03 16:46:08 -0700

    eslint fix

--- web/src/components/ProjectDashboard.js
+++ web/src/components/ProjectDashboard.js
@@ -1,18 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import { useTranslation } from 'react-i18next';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
 
 import G0vJsonGallery from './G0vJsonGallery';
 import SimilarProjectList from './SimilarProjectList';
